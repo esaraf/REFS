a420703c9cf0014dd8062a243282358c0020f89d
esaraf <elizabeth.saraf@beyondpricing.com>
2024-06-13 15:41:34 -0400

    Added Maaster Key Follow Along Option for a Correlation Test

--- Tutorial.R
+++ Tutorial.R
@@ -3,8 +3,7 @@
 ## Background context: This tutorial is only about getting
 ## a connection between your local machine and your GitHub.
 
-## It uses a tutorial dataset for running a simple linear
-## regression.
+## It uses a tutorial dataset for running a correlation test
 
 ## Don't worry if the stats get too confusing, 
 ## the purpose of this exercise is to get comfortable working
@@ -12,8 +11,11 @@
 
 ## https://www.scribbr.com/statistics/linear-regression-in-r/
 
+## TWO OPTIONS: Follow directly along with the online tutorial 
+## (simple linear regression) or practice using the Master Key 
+## from NSU's Biostatistics course running a correlation test.
 
-# ---------------- Importing Dataset -----------------------
+# ---------------- Tutorial simple linear regression -----------------------
 
 ## DOWNLOAD CSV FILE: Simple regression dataset from the URL above
 data <- read.csv("/Users/elizabethsaraf/Code/GitHub/REFS/REFS/income.data.csv") 
@@ -89,7 +91,75 @@ summary(income.happiness.lm)
 ## Commit Final Changes & Start a Pull Request
 
 
+## Summary Plot
+
+# --------------------------------------------------------------------
+
+# ---------------- Tutorial correlation test
+# Import datasset 
+dataset <- read.csv("/Users/elizabethsaraf/Code/GitHub/REFS/REFS/income.data.csv")
+attach (dataset)
+
+## Check Parametric Assumptions 
+library(lme4)
+library(car)
+scatterplot(income,happiness) # Checks linearity
+shapiro.test(income) # Shapiro-Wilks test for Normality 
+
+#Shapiro-Wilk normality test
+
+#data:  income
+#W = 0.95185, p-value = 1.159e-11
+shapiro.test(happiness)
+#Shapiro-Wilk normality test
+
+#data:  happiness
+#W = 0.98705, p-value = 0.0002095
+
+##Pearson Correlation 
+cor.test(income, happiness)
+
+# Pearson's product-moment correlation
+
+# data:  income and happiness
+# t = 38.505, df = 496, p-value < 2.2e-16
+# alternative hypothesis: true correlation is not equal to 0
+# 95 percent confidence interval:
+# 0.8417942 0.8861031
+# sample estimates:
+#       cor 
+# 0.8656337 
+
+## Summary Plot 
+
+lm(income~happiness)
+
+# Call:
+#   lm(formula = income ~ happiness)
+# 
+# Coefficients:
+#   (Intercept)    happiness  
+#     0.9053       1.0497  
+
+
+plot(happiness~income, pch=16,axes=F,xlab='',ylab='')
+axis(1,cex.axis=0.8)
+mtext(text='Income', side=1,line=3)
+axis(2,las=1)
+mtext(text='Happiness',side=2,line=3)
+box(bty='l')
+abline(lm(happiness~income))
+x <- seq(min(income),max(happiness), l=1000)
+y <- predict(lm(happiness~income), data.frame(income=x), interval='c')
+matlines(x,y,lty=2,col=1)
 
 
 
 
+
+
+
+
+
+
+ldataset <- read.csv("")
