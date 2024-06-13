bba4acbe5a7dab79da88817582353072d0f59ef4
esaraf <elizabeth.saraf@beyondpricing.com>
2024-06-13 14:22:34 -0400

    Importing & Visualizing Dataset

---
+++ Tutorial.R
@@ -0,0 +1,27 @@
+## Objective: Connect GitHub repository to your local machine.
+
+## Background context: This tutorial is only about getting
+## a connection between your local machine and your GitHub.
+
+## It uses a tutorial dataset for running a simple linear
+## regression.
+
+## Don't worry if the stats get too confusing, 
+## the purpose of this exercise is to get comfortable working
+## on your local machine and the GitHub application.
+
+## https://www.scribbr.com/statistics/linear-regression-in-r/
+
+## 
+
+
+# ---------------- Importing Dataset -----------------------
+
+## DOWNLOAD CSV FILE: Simple regression dataset from the URL above
+data <- read.csv("/Users/elizabethsaraf/Code/GitHub/REFS/REFS/income.data.csv") 
+# The path you copy in SHOULD be different than mine!
+
+head(data)
+
+## Commit changes 
+
